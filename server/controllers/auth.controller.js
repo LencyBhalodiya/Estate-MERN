3b4c91c96ef519737170c9b11d9fab945cf3adee
lency <worklency@gmail.com>
2023-11-25 16:16:32 +0530

    signup api & error middleware

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
+// import { errorHandler } from '../utils/error.js';
+
+export const signUp = async (req, res, next) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    try {
+        await newUser.save() // saving to DB
+        res.status(201).json('User created successfully!');
+    } catch (err) {
+        //next(errorHandler(500,'error from the function'));
+        next(err)
+    }
+
+}
