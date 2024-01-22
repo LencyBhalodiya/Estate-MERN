3df6088cb2b08c7e697cc52a5d80411f6f8d5949
lency <worklency@gmail.com>
2024-01-22 21:49:54 +0530

    Create index.js for all comp & pages export

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Contact} from './Contact'
+export {default as Header} from './Header'
+export {default as ListingItem} from './ListingItem'
+export {default as OAuth} from './OAuth'
+export {default as PrivateRoute} from './PrivateRoute'
+
